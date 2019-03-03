51983a8137cec529716edb2316b6308846b3582e
Logu Kirou <Logu@MacBook-Air-de-Logu.local>
2019-03-03 16:24:07 +0100

    1-End points - Added api-enpoints - Implementend validation process in controller

---
+++ src/api/v1/index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import { lotsRouter } from './resources/lots'
+import { gestionnairesRouter } from './resources/gestionnaires'
+import { clientsRouter } from './resources/clients'
+import { userRouter } from './resources/user'
+
+export const restRouterV1 = express.Router()
+restRouterV1.use('/lots', lotsRouter)
+restRouterV1.use('/gestionnaires', gestionnairesRouter)
+restRouterV1.use('/clients', clientsRouter)
+restRouterV1.use('/user', userRouter)
